79cf13e66688a70bf808979299852693d64550dc
tejasshah614 <tejas.shah@implifie.com>
2025-02-04 18:06:03 +0530

    Delete implifie_app directory

--- implifie_app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import models
-from . import res_config_settings
-from . import base_model_extension
-from . import res_partner_inherit  # Add this line
-from . import broadcast
-from . import chat
